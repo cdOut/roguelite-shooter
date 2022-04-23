23c2e48398a850614ded4d5bd870c08d9dbad6ad
Tymoteusz Kałuzieński <88325488+cdOut@users.noreply.github.com>
2022-04-23 23:38:08 +0200

    Add basic following enemy type

---
+++ Assets/Scripts/MonsterController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterController : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform playerPosition;
+
+    [Header("Movement")]
+    [SerializeField] private float monsterSpeed;
+
+    private Rigidbody _rbody;
+    private Vector3 _moveDirection = Vector3.zero;
+
+    void Awake()
+    {
+        _rbody = GetComponent<Rigidbody>();
+
+        _rbody.freezeRotation = true;
+    }
+
+    void Update()
+    {
+        if(playerPosition != null)
+            FollowPlayer ();
+    }
+
+    private void FollowPlayer() {
+        _moveDirection = (playerPosition.position - _rbody.position).normalized * monsterSpeed;
+        Debug.Log(_moveDirection);
+    }
+
+    private void FixedUpdate() {
+        _rbody.MovePosition(_rbody.position + _moveDirection * Time.fixedDeltaTime);
+    }
+}
--- Assets/Scripts/ProjectileController.cs
+++ Assets/Scripts/ProjectileController.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class ProjectileController : MonoBehaviour
 {
+    [Header("Projectile")]
+    [SerializeField] private float projectileLifeTime;
+
+    private void Start() {
+        Destroy(gameObject, projectileLifeTime);
+    }
+
     private void OnCollisionEnter(Collision collision) {
         Destroy(gameObject);
     }
